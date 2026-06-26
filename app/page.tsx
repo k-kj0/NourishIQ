a59f64b51ac779208cd7c95dbab7487afdb05d89
k-kj0 <kavya.kjais@gmail.com>
2026-06-26 14:36:00 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,4 @@
 "use client";
-
 import { useApp } from "./AppContext";
 import { OnboardingQuiz } from "./components/OnboardingQuiz";
 import { MainDashboard } from "./components/MainDashboard";
@@ -7,7 +6,6 @@ import { RecipeSheet } from "./components/RecipeSheet";
 
 export default function Home() {
   const { phase } = useApp();
-
   return (
     <>
       {phase === "onboarding" ? <OnboardingQuiz /> : <MainDashboard />}
